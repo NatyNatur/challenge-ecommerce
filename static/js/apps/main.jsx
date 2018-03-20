604c303c2ea555262822730b078578cc6d82dbc2
NatyNatur <naticcg@gmail.com>
2018-03-20 12:19:14 -0300

    mostrando productos

--- static/js/apps/main.jsx
+++ static/js/apps/main.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { catalog } from './../../../data/mock.json';
 
 const App = ({ message }) => <div>{ message }</div>;
 
+const Items = ({name, imageURL, currency, price } ) => catalog.map((data) =>
+    <div>
+        <img src={data.imageURL} alt= "imagen"/>
+        <p> {data.name} </p>
+        <p> {data.currency} {data.price} </p>
+    </div>);
+
 ReactDOM.render(<App message="hello world" />, document.getElementById('app'));
+ReactDOM.render(<Items/>, document.getElementById('app'));
